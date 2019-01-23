9f5859117217e68040168e0e8bba6a97b89f83eb
dorisdouzi <doris.douzi@gmail.com>
2019-01-23 17:32:31 +0800

    change the route

--- app/router.js
+++ app/router.js
@@ -1,8 +1,6 @@
 'use strict';
 
 module.exports = app => {
-  app.get('/', 'home.index');
-  app.post('/calculate', 'home.calculate')
-  app.get('/vba', 'vba.vba');
+  app.get('/', 'vba.vba');
   app.post('/upload', 'vba.upload');
 };
